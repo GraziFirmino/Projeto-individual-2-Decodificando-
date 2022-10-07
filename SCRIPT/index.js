3147d93aa67e077c9cd189abe8a756ba27d7c344
Grazielle Firmino <112557569+GraziFirmino@users.noreply.github.com>
2022-10-06 23:37:48 -0300

    Update index.js

--- SCRIPT/index.js
+++ SCRIPT/index.js
@@ -27,3 +27,15 @@ function criptografia() {
     toB64(text)
   }
 }
+
+function toBase64(text) {
+    const code = document.querySelector('input[name="code"]:checked').value
+    if (code === 'codificar') {
+      document.getElementById('decodedText').value = btoa(text.value)
+    } else if (code === 'decodificar') {
+      document.getElementById('decodedText').value = atob(text.value)
+    } else {
+      alert('selecione codificar ou decodificar')
+    }
+  }
+  
