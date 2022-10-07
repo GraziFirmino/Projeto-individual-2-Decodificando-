a7a3d346eba36362cb8af41883b23ff4fc1dc892
Grazielle Firmino <112557569+GraziFirmino@users.noreply.github.com>
2022-10-06 23:36:59 -0300

    Update index.js

--- SCRIPT/index.js
+++ SCRIPT/index.js
@@ -7,3 +7,23 @@ select.addEventListener('change', function () {
     steps.style.display = 'none'
   }
 })
+const radioCodificar = document.getElementById('radiocodificar')
+const radioDecodificar = document.getElementById('radiodecodificar')
+const criptografarBtn = document.getElementById('criptografarbtn')
+
+radioCodificar.addEventListener('change', function () {
+  criptografarBtn.innerHTML = 'Codificar Mensagem'
+})
+radioDecodificar.addEventListener('change', function () {
+  criptografarBtn.innerHTML = 'Decodificar Mensagem'
+})
+
+function criptografia() {
+  const text = document.getElementById('textToDecode')
+  const method = document.getElementById('selection')
+  if (method.value === 'cesar') {
+    toCaesar(text.value)
+  } else {
+    toB64(text)
+  }
+}
